8936b03cd523e5a1e5ff9e60330de8799e16d77b
Marek Hac <marekhac@hungrymedia.com>
2023-04-19 10:19:27 +0200

    Add transmission parameters

--- amiga_serial_read_async.c
+++ amiga_serial_read_async.c
@@ -19,24 +19,53 @@
 #include <devices/serial.h>
 #include <stdio.h>
 
-// size of serial read buffer
+// serial port params
 
 #define READ_BUFFER_SIZE 32
 
 struct IOExtSer *SerialIO;   /* pointer to I/O request */
 struct MsgPort  *SerialMP;   /* pointer to Message Port */
 
+BYTE serialReadBuffer[READ_BUFFER_SIZE]; /* reserve 32 bytes storage */
+
+void setupReadCommand()
+{
+    SerialIO->IOSer.io_Command = CMD_READ;
+    SerialIO->IOSer.io_Length = -1;
+    SerialIO->IOSer.io_Data = &serialReadBuffer;
+}
+
+void setupCustomSerialParams()
+{
+    // update I/O request
+
+    SerialIO->io_RBufLen = 512;
+    SerialIO->io_Baud = 9600;
+    SerialIO->io_ReadLen = 8;
+    SerialIO->io_WriteLen = 8;
+    SerialIO->io_StopBits = 1;
+    SerialIO->io_SerFlags = SERF_SHARED | SERF_XDISABLED;
+
+    // update serial parameters using SDCMD_SETPARAMS command
+
+    SerialIO->IOSer.io_Command = SDCMD_SETPARAMS;
+
+    if (DoIO(SerialIO))
+    {
+        printf("Error while setting serial parameters!\n");
+    }
+}
+
 int main(void)
 {
     ULONG signal;
-    BYTE serialReadBuffer[READ_BUFFER_SIZE]; /* reserve 32 bytes storage */
-    
+
     /* create message port */
-    
+
     if (SerialMP = (struct MsgPort *) CreatePort(0,0) )
     {
      /* create I/O request */
-        
+
         if (SerialIO=(struct IOExtSer *)
             CreateExtIO(SerialMP,sizeof(struct IOExtSer)) )
         {
@@ -45,41 +74,44 @@ int main(void)
             else
             {
                 /* device is open */
-                
+
                 SerialIO->IOSer.io_Command  = CMD_READ;
                 SerialIO->IOSer.io_Length   = -1;
                 SerialIO->IOSer.io_Data     = &serialReadBuffer;
-                
+
+                setupCustomSerialParams();
+                setupReadCommand();
+
                 /* Initiate I/O command and not wait for it to complete */
-                
+
                 SendIO(SerialIO);
-                
+
                 printf("Sleeping until CTRL-D or serial input\n");
-                
+
                 do
                 {
                     signal = Wait(1L << SerialMP -> mp_SigBit | SIGBREAKF_CTRL_D);
-                    
+
                     if (CheckIO(SerialIO) ) /* If request is complete... */
                     {
                         /* wait for specific I/O request */
-                        
+
                         WaitIO(SerialIO);
-                        
+
                         printf("%ld bytes received. ",SerialIO->IOSer.io_Actual);
                         printf("Data: %s\n", serialReadBuffer);
-                        
+
                         SendIO(SerialIO); /* restart I/O request */
                     }
                 }
                 while (!(signal & SIGBREAKF_CTRL_D));
-                
+
                 AbortIO(SerialIO);  /* ask device to abort request, if pending */
                 WaitIO(SerialIO);   /* wait for abort, then clean up */
-                
+
                 CloseDevice(SerialIO); /* close serial device */
             }
-            
+
             DeleteExtIO(SerialIO); /* delete I/O request */
         }
         else
@@ -91,6 +123,6 @@ int main(void)
     {
         printf("Unable to create message port\n");
     }
-    
+
     return 0;
 }
